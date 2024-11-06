85791592b87195765aaa3ddf072ce002e01c5c6e
MichaelChemo2507 <131193640+MichaelChemo2507@users.noreply.github.com>
2024-11-06 15:18:25 +0200

    finish ; working on the event handllers. (need testing)

--- client/src/login_and_registration/event_handllers.js
+++ client/src/login_and_registration/event_handllers.js
@@ -1,9 +1,31 @@
 export function onChangeHandller(value, userUseState, key) {
     userUseState[1]({ ...userUseState[0], [key]: value });
 }
-export function onClickHandller_login() {
-    
+export async function onClickHandller_login(userUseState, loginUseState) {
+    let response = await fetch("http://localhost:2507/users/login", {
+        method: "POST",
+        headers: {
+            'content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email: userUseState[0][0],
+            password: userUseState[0][1]
+        })
+    })
+    console.log(response);
+    alert("loged in");
 }
-export function onClickHandller_signup() {
-    
+export async function onClickHandller_signup(userUseState, loginUseState) {
+    let response = await fetch("http://localhost:2507/users/", {
+        method: "POST",
+        headers: {
+            'content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email: userUseState[0][0],
+            password: userUseState[0][1]
+        })
+    })
+    console.log(response);
+    loginUseState[1](true);
 }
